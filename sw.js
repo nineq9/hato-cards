f913fa483972bb524642ab0387ea8d9e66fab90c
nineq9 <tixia1001@gmail.com>
2026-08-14 19:02:09 +0200

    Bump KAWASEMI cache for UX polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kawasemi-v4';
+const CACHE='kawasemi-v5';
 const ASSETS=['./','./index.html','./styles.css','./articles.js','./app.js','./drag-fix.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
