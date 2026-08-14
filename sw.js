e0de4104203528978c82a6ce008f611fabe6cc40
nineq9 <tixia1001@gmail.com>
2026-08-14 20:11:53 +0200

    Bump KINGFISHER cache for intro fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kingfisher-v8';
+const CACHE='kingfisher-v9';
 const ASSETS=['./','./index.html','./styles.css','./articles.js','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
