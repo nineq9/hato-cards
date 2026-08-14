ca0dc6515f7a6ee51873d03fbe926cb588b40102
nineq9 <tixia1001@gmail.com>
2026-08-14 23:25:55 +0200

    Update KINGFISHER v14 cache manifest

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kingfisher-v13';
-const ASSETS=['./','./index.html','./kingfisher-v12.css','./news-v13.js','./kingfisher-v12.js','./manifest.webmanifest'];
+const CACHE='kingfisher-v14';
+const ASSETS=['./','./index.html','./kingfisher.css','./articles.js','./kingfisher.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin){event.respondWith(fetch(event.request));return}event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
