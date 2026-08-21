988a9bb56f6cd8d812ba3515b0d0167c53740305
nineq9 <tixia1001@gmail.com>
2026-08-21 16:29:41 +0200

    Load GEKKO tutorial images from app bundle

--- ios/be-minimal-native/be-minimal/TutorialPracticeImages.swift
+++ ios/be-minimal-native/be-minimal/TutorialPracticeImages.swift
@@ -1,19 +1,22 @@
 import UIKit
 
 enum TutorialPracticeImage: String, CaseIterable, Identifiable {
-    case tutorialPractice01
-    case tutorialPractice02
     case tutorialPractice03
+    case tutorialPractice02
+    case tutorialPractice01
 
     var id: String { rawValue }
 
+    private var resourceURL: URL {
+        Bundle.main.url(forResource: rawValue, withExtension: "jpg")!
+    }
+
     var image: UIImage {
-        UIImage(named: rawValue) ?? UIImage()
+        UIImage(contentsOfFile: resourceURL.path)!
     }
 
     var byteCount: Int64 {
-        guard let url = Bundle.main.url(forResource: rawValue, withExtension: "jpg"),
-              let values = try? url.resourceValues(forKeys: [.fileSizeKey]),
+        guard let values = try? resourceURL.resourceValues(forKeys: [.fileSizeKey]),
               let size = values.fileSize else { return 0 }
         return Int64(size)
     }
